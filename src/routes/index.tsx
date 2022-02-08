887629859eb3960945699138ae833628949fcbdf
Richardson Bruno <richardsonbrc.ads@gmail.com>
2022-02-08 19:20:37 -0300

    style: adicionado container entre o Switch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,16 +3,19 @@ import { Header } from '../components/Header'
 
 import { Home } from '../pages/Home'
 import { Cart } from '../pages/Cart'
+import { Container } from './styles'
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Header />
 
-      <Switch>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Switch>
+      <Container>
+        <Switch>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Switch>
+      </Container>
     </BrowserRouter>
   )
 }
---
+++ src/routes/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  margin: 20px auto;
+  max-width: 1140px;
+  padding: 0 15px;
+  width: 100%;
+`
