cdadf08e7b4738ef27acc89120195e56e769230f
Richardson Bruno <richardsonbrc.ads@gmail.com>
2022-02-08 23:34:04 -0300

    refactor: melhorado visualização da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,20 @@
 import { useProductProvider } from '../../hooks/useProductProvider';
 import { ProductItem } from '../../components/ProductItem/index';
+import { ListProducts } from './styles';
 
 export function Home() {
   const { products } = useProductProvider()
 
   return (
     <div>
-      {products.map((product) => {
-        return (
-          <ProductItem key={product.id} product={product} />
-        )
-      })}
+
+      <ListProducts>
+        {products.map((product) => {
+          return (
+            <ProductItem key={product.id} product={product} />
+          )
+        })}
+      </ListProducts>
     </div>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,7 @@
 import styled from 'styled-components'
+
+export const ListProducts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
+`;
