e49f0fb2cc5b8b5f6fef109c9b038cb13778c8ef
Richardson Bruno <richardsonbrc.ads@gmail.com>
2022-02-08 22:11:04 -0300

    refactor: adicionado busca no servico

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,16 @@
+import { useProductProvider } from '../../hooks/useProductProvider';
+import { ProductItem } from '../../components/ProductItem/index';
 
 export function Home() {
-  return <div>Home</div>;
+  const { products } = useProductProvider()
+
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <ProductItem key={product.id} product={product} />
+        )
+      })}
+    </div>
+  );
 }
